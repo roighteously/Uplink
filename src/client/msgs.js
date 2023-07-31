3d0b1d9ccf9aa43ab693edaf3bddf76ec9b66d76
Roizor <aidenjs9@icloud.com>
2023-07-30 23:40:15 -0500

    adsg

--- src/client/msgs.js
+++ src/client/msgs.js
@@ -1,50 +1,55 @@
-const messages = {};
-window.uplink.getMsg().then(res => {
-	res.FILES.forEach(fileDat => {
-		id = fileDat.id;
-		content = fileDat.content;
-		beforeSplitLen = content.split('\n').length;
-		content = content.replace(/=\r\n/g, "")
-		lenAfterSplit = content.split('\n').length;
-		newLenAddable = Number(beforeSplitLen - lenAfterSplit);
-		newLen = Number(Number(res.END) + newLenAddable);
+function sortMessages() {
+	const messages = {};
+	window.uplink.getMsg().then(res => {
+		res.FILES.forEach(fileDat => {
+			id = fileDat.id;
+			content = fileDat.content;
+			beforeSplitLen = content.split('\n').length;
+			content = content.replace(/=\r\n/g, "")
+			lenAfterSplit = content.split('\n').length;
+			newLenAddable = Number(beforeSplitLen - lenAfterSplit);
+			newLen = Number(Number(res.END) + newLenAddable);
 
-		messages[id] = { subject: "", body: [] }
-		const lines = content.split("\n")
-		for (let i = 0; i < lines.length; i++) {
-			let line = lines[i];
-			if (line.startsWith('Subject:')) {
-				messages[id].subject = line.split('Subject: [Uplink, at ')[1].split(']')[0];
-			}
-			if (i > lenAfterSplit - 3) {
-				if (line !== '') {
-					if(line.includes('https://')) {
-						link = line.split('https://')[1].split(' ')[0];
-						console.log(link)
-						line = `<a href="https://${link}" target="_blank">https://${link}</a>`
+			messages[id] = { subject: "", body: [] }
+			const lines = content.split("\n")
+			for (let i = 0; i < lines.length; i++) {
+				let line = lines[i];
+				if (line.startsWith('Subject:')) {
+					messages[id].subject = line.split('Subject: [Uplink, at ')[1].split(']')[0];
+				}
+				if (i > lenAfterSplit - 3) {
+					if (line !== '') {
+						if (line.includes('https://')) {
+							link = line.split('https://')[1].split(' ')[0];
+							line = `<a href="https://${link}" target="_blank">https://${link}</a>`
+						}
+						if (line.includes('.jpeg') || line.includes('.jpg') || line.includes('.gif') || line.includes('.png')) {
+							link = line.split('https://')[1].split(' ')[0];
+							line = `<img src="https://${link}" alt="Uplink Image">`
+						}
+						messages[id].body.push(line.trim());
 					}
-					messages[id].body.push(line.trim());
 				}
 			}
-		}
-	})
-}).finally(() => {
-	let subject;
-	let body;
-	Object.keys(messages).forEach(msgID => {
-		subject = messages[msgID].subject;
-		body = messages[msgID].body.join('<br>');
-		const msg = document.createElement('div');
-		const subj = document.createElement('h1');
-		const bdy = document.createElement('p');
-		subj.innerText = subject;
-		// body.split('https://')[1].split(' ')[0];
-		bdy.innerHTML = body;
-		msg.className = 'msg';
+		})
+	}).finally(() => {
+		let subject;
+		let body;
+		Object.keys(messages).forEach(msgID => {
+			subject = messages[msgID].subject;
+			body = messages[msgID].body.join('<br>');
+			const msg = document.createElement('div');
+			const subj = document.createElement('h1');
+			const bdy = document.createElement('p');
+			subj.innerText = subject;
+			// body.split('https://')[1].split(' ')[0];
+			bdy.innerHTML = body;
+			msg.className = 'msg';
 
-		msg.appendChild(subj)
-		msg.appendChild(bdy);
+			msg.appendChild(subj)
+			msg.appendChild(bdy);
 
-		document.querySelector('.flex').appendChild(msg);
+			document.querySelector('.flex').appendChild(msg);
+		})
 	})
-})
+}
