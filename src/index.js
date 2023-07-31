6bd1d46c9c0a6f433ef635eed7ddfc25f765a1f0
Roizor <aidenjs9@icloud.com>
2023-07-30 21:36:39 -0500

    code more

--- src/index.js
+++ src/index.js
@@ -1,62 +1,53 @@
 const Imap = require('node-imap');
-const inspect = require('util').inspect;
 
 const cfg = require('../config.json');
+const fs = require('fs')
 
 var imap = new Imap({
-  user: cfg.imap.username,
-  password: cfg.imap.password,
-  host: cfg.imap.host,
-  port: cfg.imap.port,
-  tls: cfg.imap.tls
+	user: cfg.imap.username,
+	password: cfg.imap.password,
+	host: cfg.imap.host,
+	port: cfg.imap.port,
+	tls: cfg.imap.tls
 });
- 
+
 function openInbox(cb) {
-  imap.openBox('INBOX', true, cb);
+	imap.openBox('INBOX', true, cb);
 }
- 
-imap.once('ready', function() {
-  openInbox(function(err, box) {
-    if (err) throw err;
-    var f = imap.seq.fetch('1:3', {
-      bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-      struct: true
-    });
-    f.on('message', function(msg, seqno) {
-      console.log('Message #%d', seqno);
-      var prefix = '(#' + seqno + ') ';
-      msg.on('body', function(stream, info) {
-        var buffer = '';
-        stream.on('data', function(chunk) {
-          buffer += chunk.toString('utf8');
-        });
-        stream.once('end', function() {
-          console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-        });
-      });
-      msg.once('attributes', function(attrs) {
-        console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-      });
-      msg.once('end', function() {
-        console.log(prefix + 'Finished');
-      });
-    });
-    f.once('error', function(err) {
-      console.log('Fetch error: ' + err);
-    });
-    f.once('end', function() {
-      console.log('Done fetching all messages!');
-      imap.end();
-    });
-  });
+
+imap.once('ready', function () {
+	openInbox(function (err, box) {
+		if (err) throw err;
+		imap.search([['SUBJECT', '[Uplink']], function (err, results) {
+			if (err) throw err;
+			var f = imap.fetch(results, { bodies: '' });
+			f.on('message', function (msg, seqno) {
+				msg.on('body', function (stream, info) {
+					stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+				});
+			});
+			f.once('error', function (err) {
+				console.log('Fetch error: ' + err);
+			});
+			f.once('end', function () {
+				console.log('Done fetching all messages!');
+				imap.end();
+			});
+		});
+	});
 });
- 
-imap.once('error', function(err) {
-  console.log(err);
+
+imap.once('error', function (err) {
+	console.log(err);
 });
- 
-imap.once('end', function() {
-  console.log('Connection ended');
+
+imap.once('end', function () {
+	console.log('Got Uplink data');
+	start();
 });
- 
+
 imap.connect();
+
+function start() {
+
+}
